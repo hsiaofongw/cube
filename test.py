a56f7fe60141bc3d030ecf226fef4659aa4e272e
hsiaofongw <hsiaofong.w@gmail.com>
2021-01-26 21:56:00 +0800

    update

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from helper import Helper
-from math import cos
+from math import cos, dist
 from scipy.spatial.distance import cosine as cosine_distance
 
 class TestHelperMethods(unittest.TestCase):
@@ -88,51 +88,72 @@ class TestHelperMethods(unittest.TestCase):
     
     def test_check_intersect(self):
  
-        pass
-        # n_positives = 100
-        # points_a = np.random.rand(n_positives, 3)
-        # points_b = np.random.rand(n_positives, 3)
-        # points_c = np.random.rand(n_positives, 3)
-        # vecs_ab = points_b - points_a
-        # vecs_ac = points_c - points_a
-        # points_o = np.random.rand(n_positives, 3)
-        # args_z = np.random.rand(n_positives, 1)
-        # args_x = np.random.rand(n_positives, 1)
-        # args_y = np.random.rand(n_positives, 1)
+        n_samples = 100
+        scale_factor = 100
+        points_o = np.random.rand(n_samples, 3) * scale_factor
+        points_a = np.random.rand(n_samples, 3) * scale_factor
+        points_b = np.random.rand(n_samples, 3) * scale_factor
+        points_c = np.random.rand(n_samples, 3) * scale_factor
+        vecs_ba = points_b - points_a
+        vecs_ca = points_c - points_a
 
-        # point_o = np.random.rand(3)
-        # v = np.random.rand(3)
-        # point_a = np.random.rand(3)
-        # point_b = np.random.rand(3)
-        # point_c = np.random.rand(3)
+        true_xs = np.random.rand(n_samples, 1)
+        true_ys = np.random.rand(n_samples, 1)
+        points_p = points_a + true_xs * vecs_ba + true_ys * vecs_ca
+        distances = np.linalg.norm(points_p - points_o, axis=1).reshape(n_samples, 1)
+        portions = np.random.rand(n_samples, 1)
+        vecs_v = points_p - points_o
+        vecs_v_norm = np.linalg.norm(vecs_v, axis=1).reshape(vecs_v.shape[0], 1)
+        vecs_v = vecs_v / vecs_v_norm
+        v_lengths = distances * portions
+        vecs_v = v_lengths * vecs_v
+        true_zs = distances / v_lengths
 
-        # mat_Aeq = np.zeros(shape=(1, 2,), dtype=np.float)
-        # mat_Beq = np.zeros(shape=(1, 2,), dtype=np.float)
+        mat_Aeq = np.zeros(shape=(1, 2,), dtype=np.float)
+        mat_Beq = np.zeros(shape=(2, 1,), dtype=np.float)
 
-        # mat_Aleq = np.array([
-        #     [1, 1],
-        #     [-1, 0],
-        #     [0, -1]
-        # ])
-        # mat_Bleq = np.array([
-        #     [1],
-        #     [0],
-        #     [0]
-        # ])
+        mat_Aleq = np.array([
+            [1, 1],
+            [-1, 0],
+            [0, -1]
+        ])
+        mat_Bleq = np.array([
+            [1],
+            [0],
+            [0]
+        ])
 
-        # result = Helper.check_intersect(
-        #     point_o, 
-        #     v, 
-        #     point_a, 
-        #     point_b, 
-        #     point_c, 
-        #     mat_Aeq, 
-        #     mat_Beq, 
-        #     mat_Aleq, 
-        #     mat_Bleq
-        # )
+        computed_xs = np.zeros_like(true_xs)
+        computed_ys = np.zeros_like(true_ys)
+        computed_zs = np.zeros_like(true_zs)
+        for i in range(n_samples):
+            point_o = points_o[i, :]
+            v = vecs_v[i, :]
+            point_a = points_a[i, :]
+            point_b = points_b[i, :]
+            point_c = points_c[i, :]
+            check, answer = Helper.check_intersect(
+                point_o, 
+                v, 
+                point_a, 
+                point_b, 
+                point_c, 
+                mat_Aeq, 
+                mat_Beq, 
+                mat_Aleq, 
+                mat_Bleq
+            )
+            computed_zs[i, 0] = answer[0, 0]
+            computed_xs[i, 0] = answer[1, 0]
+            computed_ys[i, 0] = answer[2, 0]
+        
+        true_values = np.concatenate((true_zs, true_xs, true_ys,), axis=1)
+        computed_values = np.concatenate((computed_zs, computed_xs, computed_ys,), axis=1)
 
-        # print(result)
+        eps = 1e-6
+        errors = np.abs(true_values - computed_values)
+        max_error = np.max(errors)
+        self.assertAlmostEqual(max_error, 0.0, delta=eps)
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored', '-v'])
